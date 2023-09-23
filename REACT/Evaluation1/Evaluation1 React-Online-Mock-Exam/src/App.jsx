406ba7a2bb54c332b82633efcf876680c79c0b59
Shubham10320 <chaubeyshubh1032@gmail.com>
2023-09-23 21:53:29 +0530

    react c1 evalutaion

---
+++ REACT/Evaluation1/Evaluation1 React-Online-Mock-Exam/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Booklet from "./Components/Booklet";
+function App() {
+  return <div className="App">
+    {/* import Booklet component here */}
+    <Booklet/>
+  </div>;
+}
+
+export default App;
